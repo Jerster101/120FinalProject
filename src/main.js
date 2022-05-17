b96c39091db1f4f15d76c5a8a24854ed8f09992f
Sherwatt <79425724+Sherwatt@users.noreply.github.com>
2022-05-16 18:02:30 -0700

    Update main.js
    Added in some constant variables, and the ability to switch between scenes with a button press.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
+//making some permanent variables so I don't have to keep doing "this.something"
+let cursors;
 const tileSize = 35;
+const cloudSize = 65;
+var SCALE = 0.5,
+ACCELERATION = 500,
+DRAG = 600,
+MAX_X_VEL = 500,
+MAX_Y_VEL = 5000;
 
 let config = {
     type: Phaser.CANVAS,
@@ -13,9 +21,26 @@ let config = {
             debug: false,
         }
     },
-    scene: [ Load, Gamescreen ]
+    scene: [ Load, Coinscreen, Platformscreen ]
 }
 
 let game = new Phaser.Game(config);
 
 let keyLEFT, keyRIGHT, keyA, keyD, keySPACE, keyESC;
+
+let sceneSwitcher = (event) => {
+    //console.log("Key is: " + event.key);
+    switch(event.key) {
+        case '1':
+            game.scene.start('Level1');
+            game.scene.bringToTop('Level1');
+            game.scene.pause('Level2');
+            break;
+        case '2':
+            game.scene.start('Level2');
+            game.scene.bringToTop('Level2');
+            game.scene.pause('Level1');
+        default:
+            break;
+    }
+}
