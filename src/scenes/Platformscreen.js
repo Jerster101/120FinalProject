f235146a0cbccfcbbd925358eb066f5a813d5512
Sherwatt <79425724+Sherwatt@users.noreply.github.com>
2022-05-16 18:10:18 -0700

    Create Platformscreen.js
    New screen I made to show off the various unique platform types, it's not finished yet, but I plan on there being 3 types, the bouncy platforms on the bottom, the clouds, which you can jump through underneath and land on the top, and crumbling platforms that you can stay on for a few seconds before they fall.

---
+++ src/scenes/Platformscreen.js
@@ -0,0 +1,67 @@
+class Platformscreen extends Phaser.Scene {
+    constructor() {
+        super("Level2");
+    }
+
+    create() {
+
+        this.cameras.main.setBackgroundColor('#227B96');
+
+        this.clouds = this.add.group();
+        for(let i = 0; i < game.config.width; i += cloudSize) {
+            let cloudTile = this.physics.add.sprite(i, game.config.height - cloudSize*3, 'platformer_atlas', 'cloud_3').setScale(SCALE);
+            cloudTile.body.immovable = true;
+            cloudTile.body.allowGravity = false;
+            this.clouds.add(cloudTile);
+        }
+        //makes a group of tiles that the player can bounce on
+        this.bouncy_ground = this.add.group();
+        for(let i = 0; i < game.config.width; i += tileSize) {
+            let groundTile = this.physics.add.sprite(i, game.config.height - tileSize, 'platformer_atlas', 'ground_rock').setScale(SCALE);
+            groundTile.body.immovable = true;
+            groundTile.body.allowGravity = false;
+            this.bouncy_ground.add(groundTile);
+        }
+
+        this.player = this.physics.add.sprite(game.config.width/2, game.config.height/3, 'platformer_atlas', 'front').setScale(SCALE);
+        this.player.setMaxVelocity(MAX_X_VEL, MAX_Y_VEL);
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+        //allows us to switch scenes
+        this.input.keyboard.on('keydown', sceneSwitcher);
+    }
+
+    update() {
+        //player movements and animations
+        if(this.cursors.left.isDown) {
+            this.player.body.setAccelerationX(-ACCELERATION);
+            this.player.setFlip(true, false);
+            this.player.anims.play('walk', true);
+
+        } else if(this.cursors.right.isDown) {
+            this.player.body.setAccelerationX(ACCELERATION);
+            this.player.resetFlip();
+            this.player.anims.play('walk', true);
+
+        } else {
+            this.player.body.setAccelerationX(0);
+            this.player.body.setDragX(DRAG);
+            this.player.anims.play('idle');
+
+        }
+
+        if(!this.player.body.touching.down) {
+            this.player.anims.play('jump', true);
+        }
+        //jumping
+        if(this.player.body.touching.down && Phaser.Input.Keyboard.JustDown(this.cursors.up)) {
+            this.player.setVelocityY(-1000);
+        }
+        this.physics.world.wrap(this.player, 0);
+
+        //bounce collision
+        this.player.setBounce(1, 1);
+        this.physics.collide(this.player, this.bouncy_ground);
+        this.player.setBounce(0, 0);
+    }
+}
