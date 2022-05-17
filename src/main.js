5e1f7875857510e270150e0f45c4a7f94871c4d5
Lyza Stevens <91556557+lyza13@users.noreply.github.com>
2022-05-17 01:50:09 -0700

    add camera + tile map

--- src/main.js
+++ src/main.js
@@ -13,25 +13,29 @@ const SCALE = 1.2;
 const GRAV = 1000;
 const DRAG = 950;
 const MOVESPEED = 300;
-const JUMPHEIGHT = 5000;
+const JUMPHEIGHT = 500;
 
 let config = {
     type: Phaser.CANVAS,
-    width: 1500,
-    height: 800,
+    width: 640,
+    height: 360,
     autoCenter: true,
-    backgroundColor: '#8a8a8a',
+    backgroundColor: '#C4ADA8',
     autoCenter: true,
+    pixelArt: true,
+    zoom: 2,
     physics: {
         default: 'arcade',
         arcade: {
-            debug: false,
+            debug: true,
+            tileBias: 32,
         }
     },
     scene: [Menu, Death, Pause, Level1]
 }
 
 let game = new Phaser.Game(config);
+
 
 // set UI sizes
 let borderUISize = game.config.height / 20;
