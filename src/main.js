b6c5536838e74a93dc0f609a8ded233c85f8d1df
Sherwatt <79425724+Sherwatt@users.noreply.github.com>
2022-05-19 00:23:46 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,14 +4,14 @@ const tileSize = 35;
 const cloudSize = 65;
 var SCALE = 0.5,
 ACCELERATION = 500,
-DRAG = 950,
+DRAG = 900,
 MAX_X_VEL = 500,
 MAX_Y_VEL = 5000;
 
 let config = {
     type: Phaser.CANVAS,
-    width: 640,
-    height: 360,
+    width: 1280,
+    height: 640,
     backgroundColor: '#B7410E',
     autoCenter: true,
     physics: {
@@ -29,19 +29,21 @@ let game = new Phaser.Game(config);
 let sceneSwitcher = (event) => {
     //console.log("Key is: " + event.key);
     switch(event.key) {
+        /*can't get this page to work yet, so I'm gonna just block off access until I can get it to work
         case '1':
             game.scene.start('Level1');
             game.scene.bringToTop('Level1');
             game.scene.pause('Tutorial1');
             game.scene.pause('Tutorial2');
             break;
-        case '2':
+            */
+        case '1':
             game.scene.start('Tutorial1');
             game.scene.bringToTop('Tutorial1');
             game.scene.pause('Level1');
             game.scene.pause('Tutorial2');
             break;
-        case '3':
+        case '2':
             game.scene.start('Tutorial2');
             game.scene.bringToTop('Tutorial2');
             game.scene.pause('Level1');
