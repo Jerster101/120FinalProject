f86a898d24f9b592663e17f4c5ee1665998f0cd1
Sherwatt <79425724+Sherwatt@users.noreply.github.com>
2022-05-21 23:56:58 -0700

    Update Load.js

--- src/prefabs/Load.js
+++ src/prefabs/Load.js
@@ -8,9 +8,8 @@ class Load extends Phaser.Scene {
         this.load.path = "assets/";
         //this is where all permanent assets will go
         this.load.atlas('platformer_atlas', 'kenny_sheet.png', 'kenny_sheet.json');
-        this.load.image('groundScroll', 'ground.png');
-        this.load.image('tall_trees', 'talltrees.png');
         this.load.image('star', 'star.png');
+        this.load.image('duskSky', 'mountain-bg.png');
         //stuff for level 1
         this.load.image('tiles', 'red_ground.png');
         this.load.tilemapTiledJSON('map', 'red_map.json')
